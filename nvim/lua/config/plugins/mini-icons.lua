295e9a86f91aee58f031481797a0a21848fb505b
Rich Lee <rich@richmlee.com>
2024-07-13 17:40:29 -0400

    Update nvim plugins

---
+++ nvim/lua/config/plugins/mini-icons.lua
@@ -0,0 +1,8 @@
+return {
+  'echasnovski/mini.icons', version = false,
+  config = function()
+    local mini_icons = require('mini.icons')
+
+    mini_icons.setup()
+  end
+}
--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -2,4 +2,8 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.3',
   -- or                              , branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local telescope = require('telescope')
+    telescope.load_extension('nerdy')
+  end
 }
