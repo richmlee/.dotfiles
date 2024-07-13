6c5d0cef7b9eb4e09aae58c8158d00c3ea2209ea
Rich Lee <rich@richmlee.com>
2024-07-13 15:47:10 -0400

    Add scripts for mac setup

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# tpm https://github.com/tmux-plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+~/.config/tmux/plugins/tpm/bin/install_plugins
