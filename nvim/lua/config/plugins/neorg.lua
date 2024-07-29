98f0cf9ab9281ea0370e2314d8fda749ba0302eb
Rich Lee <richard.lee@procore.com>
2024-07-29 01:54:30 -0400

    neorg and tmuxp setups

--- nvim/lua/config/plugins/neorg.lua
+++ nvim/lua/config/plugins/neorg.lua
@@ -11,11 +11,21 @@ return {
         ["core.dirman"] = {
           config = {
             workspaces = {
-              notes = "~/notes",
+              notes = "~/notes", -- root, but should not keep files here?
+              work = "~/notes/work",
+              work_notes = "~/notes/work/notes",
+              work_journal = "~/notes/work/journal",
+              work_todos = "~/notes/work/todos",
+              personal = "~/notes/personal",
+              peronal_notes = "~/notes/personal/notes",
+              personal_journal = "~/notes/personal/journal",
+              personal_todos = "~/notes/personal/todos",
             },
           },
         },
       }
     })
+
+    vim.opt.conceallevel = 2
   end,
 }
