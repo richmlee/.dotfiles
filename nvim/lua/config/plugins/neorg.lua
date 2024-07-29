8be87528c3d6a362d4133f04f866460602255b8b
Rich Lee <richard.lee@procore.com>
2024-07-28 20:20:40 -0400

    add neorg settings

--- nvim/lua/config/plugins/neorg.lua
+++ nvim/lua/config/plugins/neorg.lua
@@ -2,5 +2,12 @@ return {
   "nvim-neorg/neorg",
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
-  config = true,
+  config = function()
+    local neorg = require("neorg")
+    neorg.setup({
+      load = {
+        ["core.defaults"] = {},
+      }
+    })
+  end,
 }
