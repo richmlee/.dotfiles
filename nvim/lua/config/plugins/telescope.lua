b00eff96065b6765a3e6a0b283ae55c09a6f19a1
Rich Lee <richard.lee@procore.com>
2024-08-03 00:14:55 -0400

    work neovim config updates

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -9,18 +9,20 @@ return {
   config = function()
     local telescope = require("telescope")
 
-    telescope.load_extension("nerdy")
-		telescope.load_extension("file_browser")
-
-		local builtin = require("telescope.builtin")
-
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
     telescope.setup({
-      path_display = { "truncate" },
+      defaults = {
+        path_display = { "truncate" },
+      },
     })
+
+    telescope.load_extension("nerdy")
+    telescope.load_extension("file_browser")
+
+    local builtin = require("telescope.builtin")
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
   end,
 }
